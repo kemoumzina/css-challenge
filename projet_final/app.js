28ce2b32a71451cbd5d16a36c0e34dafdf290ae4
kemoum zina <zinacetic@gmail.com>
2024-05-14 00:50:39 +0100

    premier commit

---
+++ projet_final/app.js
@@ -0,0 +1,43 @@
+console.log("hello")
+// dom
+
+const touches = [...document.querySelectorAll('.bouton')];
+const listkeycode =touches.map(touche=>touche.dataset.key);
+console.log(touches,listkeycode);
+const ecran = document.querySelector('.ecran');
+
+document.addEventListener('keydown',(e) => {
+    console.log(e);
+    const valeur = e.keycode.toString();
+  calculer(valeur)
+})
+ 
+document.addEventListener('click',(e) => {
+    console.log(e);
+    const valeur = e.target.dataset.key;
+    console.log(valeur);
+    calculer(valeur);
+})
+
+
+const calculer=(valeur) => {
+    if (listkeycode.includes(valeur)) {
+        console.log(listkeycode);
+        switch (valeur) {
+            case '8':
+                ecran.textContent="";
+                break;
+            case '13':
+                const calcul = eval(ecran.textContent);
+                ecran.textContent=calcul;
+                break;
+            default :
+                const indexKeycode = listkeycode.indexOf(valeur);
+               const touche= touches[indexKeycode]; 
+             
+                ecran.textContent+=touche.innerHTML;
+
+        }
+    }
+}
+window.addEventListener('error',(e)=>console.log())
